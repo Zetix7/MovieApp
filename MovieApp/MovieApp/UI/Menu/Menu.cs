736db9ec1ec4a9206d485d82e37f1bd4c7d94631
Greg Zieliński <120790228+Zetix7@users.noreply.github.com>
2024-01-01 18:02:11 +0100

    Change LoadMenu method in Menu class

--- MovieApp/MovieApp/UI/Menu/Menu.cs
+++ MovieApp/MovieApp/UI/Menu/Menu.cs
@@ -1,9 +1,17 @@
 ﻿using MovieApp.DataAccess.Data.Entities;
+using MovieApp.DataAccess.Data.Repositories;
 
 namespace MovieApp.UI.Menu;
 
-public class Menu<T> : IMenu<T> where T : class, IEntity
+public class Menu<T> : IMenu<T> where T : class, IEntity, new()
 {
+    private readonly IRepository<T> _repository;
+
+    public Menu(IRepository<T> repository)
+    {
+        _repository = repository;
+    }
+
     public void LoadMenu()
     {
         Console.WriteLine($"\n------- {typeof(T).Name}s Menu -------\n");
@@ -13,9 +21,14 @@ public class Menu<T> : IMenu<T> where T : class, IEntity
         {
             Console.WriteLine("-----------------------------------------------------------------------");
             Console.WriteLine("Choose one option:");
-            Console.WriteLine($"\t1. {typeof(T).Name}s CRUD actions in repository.");
-            Console.WriteLine($"\t2. {typeof(T).Name}s *.csv file actions.");
-            Console.WriteLine($"\t3. {typeof(T).Name}s *.xml file actions.");
+            Console.WriteLine($"\t1. Get all {typeof(T).Name}s from repository.");
+            Console.WriteLine($"\t2. Get by Id {typeof(T).Name} from repository.");
+            Console.WriteLine($"\t3. Add new {typeof(T).Name} to repository.");
+            Console.WriteLine($"\t4. Remove exists {typeof(T).Name} in repository.");
+            Console.WriteLine($"\t5. Create {typeof(T).Name.ToLower()}s.csv file from repository.");
+            Console.WriteLine($"\t6. Read all {typeof(T).Name.ToLower()}s from {typeof(T).Name}s.csv file.");
+            Console.WriteLine($"\t7. Create {typeof(T).Name.ToLower()}s.xml file from repository.");
+            Console.WriteLine($"\t8. Read all {typeof(T).Name.ToLower()}s from {typeof(T).Name}s.xml file.");
             Console.WriteLine("\tQ. Return.");
             Console.Write("\t\tYour choise: ");
             choise = Console.ReadLine()!.Trim().ToUpper();
@@ -28,6 +41,16 @@ public class Menu<T> : IMenu<T> where T : class, IEntity
                 case "2":
                     break;
                 case "3":
+                    break;
+                case "4":
+                    break;
+                case "5":
+                    break;
+                case "6":
+                    break;
+                case "7":
+                    break;
+                case "8":
                     break;
                 case "Q":
                     break;
