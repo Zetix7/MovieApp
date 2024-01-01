8d2cc8ca1c0d610029c86a85482423be792844c6
Greg Zieliński <120790228+Zetix7@users.noreply.github.com>
2024-01-01 13:08:09 +0100

    Added validation in CsvReader class

--- MovieApp/MovieApp/AplicationServices/Components/FileCreator/CsvFile/CsvReader.cs
+++ MovieApp/MovieApp/AplicationServices/Components/FileCreator/CsvFile/CsvReader.cs
@@ -7,18 +7,35 @@ public class CsvReader : ICsvReader
 {
     public List<Movie> ReadMovieCsvFile()
     {
-        var movies = File.ReadAllLines(@"DataAccess\Resources\Files\movies.csv").Where(x => x.Length > 0).Select(x => 
+        if (!File.Exists(@"DataAccess\Resources\Files\movies.csv"))
         {
-            var movie = x.Split(',');
-            return new Movie
-            {
-                Title = movie[0],
-                Year = int.Parse(movie[1], CultureInfo.InvariantCulture),
-                Universe = movie[2],
-                BoxOffice = decimal.Parse(movie[3], CultureInfo.InvariantCulture),
-            };
-        }).ToList();
+            throw new FileNotFoundException("File 'movies.csv' not exists!");
+        }
 
-        return movies;
+        if(new FileInfo(@"DataAccess\Resources\Files\movies.csv").Length < 10)
+        {
+            throw new FileLoadException("File 'movies.csv' is empty!");
+        }
+
+        try
+        {
+            var movies = File.ReadAllLines(@"DataAccess\Resources\Files\movies.csv").Where(x => x.Length > 0).Select(x =>
+            {
+                var movie = x.Split(',');
+                return new Movie
+                {
+                    Title = movie[0],
+                    Year = int.Parse(movie[1], CultureInfo.InvariantCulture),
+                    Universe = movie[2],
+                    BoxOffice = decimal.Parse(movie[3], CultureInfo.InvariantCulture),
+                };
+            }).ToList();
+
+            return movies;
+        }
+        catch (Exception)
+        {
+            throw new Exception("File 'movies.csv' is broken!");
+        }
     }
 }
