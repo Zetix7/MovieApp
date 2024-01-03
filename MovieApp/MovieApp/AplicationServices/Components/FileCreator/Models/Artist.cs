6093f3203ac80567557feea65aef3f4481aee3cd
Greg Zieliński <120790228+Zetix7@users.noreply.github.com>
2024-01-03 19:03:04 +0100

    Created model for files Artist class in FileCreator\Models folder

---
+++ MovieApp/MovieApp/AplicationServices/Components/FileCreator/Models/Artist.cs
@@ -0,0 +1,9 @@
+﻿namespace MovieApp.AplicationServices.Components.FileCreator.Models;
+
+public class Artist
+{
+    public string? FirstName { get; set; }
+    public string? LastName { get; set; }
+
+    public override string ToString() => $"Name: {FirstName} {LastName}";
+}
